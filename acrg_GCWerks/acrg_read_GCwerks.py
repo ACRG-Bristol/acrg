b070c3dd3ab8904cbf056f6a08c4b729c6dc76a9
as13988it <ann.stavert@bristol.ac.uk>
2015-06-22 17:13:05 +0100

    Updated GCWerks read function so that it can now read .C files and the automatically created GCWerks output AS

--- acrg_GCWerks/acrg_read_GCwerks.py
+++ acrg_GCWerks/acrg_read_GCwerks.py
@@ -9,7 +9,7 @@ import os
 import numpy as np
 import datetime as dt
 import pdb
-
+import fnmatch
  # Class to read in the txt output of gcexport made using the standard peak.list and report config on Dagage2
 def read_gcexport_crds(datafile):
         
@@ -20,12 +20,19 @@ def read_gcexport_crds(datafile):
         elif type(datafile) == str:
             data=np.genfromtxt(datafile, dtype=str, skip_header=3)
         
-               
+        dirname, filename = os.path.split(datafile)        
         
-        if np.shape(data)[1] > 18 :
-            data = read_gcexport_crds_new(data, datafile)
+        if fnmatch.fnmatch(dirname, '/dagage2/agage/*'):
+            if fnmatch.fnmatch(filename, '*.dat'):
+                data = read_gc_stdoutput(data, datafile)
+            else:
+                data = read_dotC(data, datafile)
+                
         else:
-            data = read_gcexport_crds_old(data, datafile)
+            if np.shape(data)[1] > 18 :
+                data = read_gcexport_crds_new(data, datafile)
+            else:
+                data = read_gcexport_crds_old(data, datafile)
             
             
         return data
@@ -240,6 +247,132 @@ class read_gcexport_crds_new:
         self.filename = filename
         self.datadir = dirname
 
+#Created: 22 Jun 15 06:34 GMT
+#     -      -         -    -       ch4     ch4   ch4       co2     co2   co2        co      co    co 
+#  date   time      type port         C   stdev     N         C   stdev     N         C   stdev     N 
+#131120 124830       air   10       nan     nan   nan       nan     nan   nan       nan     nan   nan 
+class read_gc_stdoutput:
+    def __init__(self, data, datafile):
+        
+        date = data[:,0] # YYMMDD
+        time = data[:,1] # HHMMSS
+        sampletype = data[:,2]
+        port = data[:,3]
+        ch4_orig = data[:,4]
+        ch4sd_orig = data[:,5]
+        ch4_n = data[:,6]
+        co2_orig = data[:,7] 
+        co2sd_orig = data[:,8]
+        co2_n = data[:,9]
+
+         
+        dirname, filename = os.path.split(datafile)
+
+        # These data do not appear to contain flags
+        
+        # CO data is only at some sites
+        if np.shape(data)[1] == 13:
+            co_orig = data[:,10]
+            cosd_orig = data[:,11]
+            co_n = data[:,12]
+            
+            
+        # make datetime variable
+        dt_date = [dt.datetime.strptime(date[i] + ' ' + time[i] , "%y%m%d %H%M%S") for i in np.arange(len(date))]
+           
+        self.date = date
+        self.time = time
+        self.dt_date = dt_date
+        self.datetime = dt_date
+        self.sampletype = sampletype
+        self.port = port.astype('int')
+        
+        self.co2_orig = co2_orig
+        self.co2 = co2_orig
+        self.co2sd_orig = co2sd_orig
+        self.co2sd = co2sd_orig
+        self.co2flags = ''
+        self.co2_n = co2_n
+        
+        self.ch4_orig = ch4_orig
+        self.ch4 = ch4_orig
+        self.ch4sd_orig = ch4sd_orig
+        self.ch4sd = ch4sd_orig
+        self.ch4flags = ''
+        self.ch4_n = ch4_n
+        
+        self.nogases = 2        
+        
+        # CO data is only at some sites
+        if data[0][-1] != 'nan':
+            self.co_orig = co_orig
+            self.co = co_orig
+            self.cosd_orig = cosd_orig
+            self.cosd = cosd_orig
+            self.coflags = ''
+            self.co_n = co_n
+            
+            self.nogases = 3
+        
+        self.filename = filename
+        self.datadir = dirname
+
+#Created: 22 Jun 15 06:34 GMT
+#     -      -         -    -       ch4     ch4   ch4       co2     co2   co2        co      co    co 
+#  date   time      type port         C   stdev     N         C   stdev     N         C   stdev     N 
+#131120 124830       air   10       nan     nan   nan       nan     nan   nan       nan     nan   nan 
+class read_dotC:
+    def __init__(self, data, datafile):
+        
+        data = np.genfromtxt(datafile, dtype = str, skip_header=2) 
+        
+        scales = data[0,:]        
+        units = data[1,:]
+        header = data[2,:]
+        
+        data = data[3:,:]     
+        
+        decdate = data[:,0].astype(float)
+        year = data[:,1].astype(int) 
+        month = data[:,2].astype(int)
+        day = data[:,3].astype(int)
+        hour = data[:,4].astype(int)
+        minute = data[:,5].astype(int)
+        sampletype = data[:,6]
+        standard = data[:,7] 
+        
+        # pattern is then gas name then flag
+        tags = header[8:]
+        gases = tags[np.arange(len(tags)/2)*2]
+        flagtags = [i + '_flags' for i in gases]
+        tags[np.arange(len(tags)/2)*2+1] = flagtags
+        
+        outdict = {}
+        for i in np.arange(np.shape(data)[1] - 8):
+            
+            if i/2 - np.floor(i/2) > 0 :          
+                outdict[tags[i]] = data[:,i].astype(float)       
+            else:
+                outdict[tags[i]] = data[:,i]
+                    
+        dirname, filename = os.path.split(datafile)
+            
+        # make datetime variable
+        dt_date = [dt.datetime(year[i], month[i], day[i], hour[i], minute[i]) for i in np.arange(len(year))]
+           
+        self.decdate = decdate
+        self.dt_date = dt_date
+        self.datetime = dt_date
+        self.sampletype = sampletype
+        self.standard = standard
+        self.scales = scales[8:]
+        self.units = units[8:]
+        self.data = outdict
+        self.datakeys = tags
+        self.filename = filename
+        self.datadir = dirname
+
+
 # Class to make the flags
 # nb: an * flag will =  a 1 flag
 # an F flag will = a 2 flag
@@ -306,3 +439,4 @@ def Extractgood(time, gas, flags, SD=0):
         sd_flag = 0
         
     return time_flag, gas_flag, sd_flag
+
