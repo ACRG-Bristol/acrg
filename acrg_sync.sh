0c9bff8817b92e32a10e2664fdfda8c4b8ea693a
Matt <matt.rigby@bristol.ac.uk>
2015-09-02 14:40:19 +0100

    Changed acrg_agage

--- acrg_sync.sh
+++ acrg_sync.sh
@@ -3,14 +3,15 @@
 air_shared_folder="/data/shared/"
 snowy_shared_folder="/shared_data/snowy/shared/"
 
-
 # Air to Snowy, one way
 ##############################################
 
 folders="NAME/basis_functions
-         NAME/boundary_conditions
+         NAME/bc
          NAME/emissions
-         NAME/fp_netcdf"
+         NAME/fp
+         NAME/bc_basis_functions
+         NAME/countries"
 
 for f in $folders
 do
