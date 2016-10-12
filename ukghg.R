8d5939b6a774a29238062e5b9d808da3eaab6613
bs15965 <bs15965@air.chm.bris.ac.uk>
2016-10-12 15:30:00 +0100

    Small edit for clarification

--- ukghg.R
+++ ukghg.R
@@ -10,14 +10,21 @@ nTimes <- as.integer(myArgs[4])
 # create a sequence of timestamps
 datect <- seq(startDate, endDate, length = nTimes)
 # calculate fluxes for these times
-if(myArgs[10]=='combined') myFlux <- calcFlux(myArgs[5], datect, myArgs[6], myArgs[7], myArgs[8],sectorList = ifelse(myArgs[9]=='None',1:10,myArgs[9]))
+if(myArgs[9]=='None'){
+  sectorList <- c(1:10)
+} else ∂{
+  sectorList <- myArgs[9]
+}
+
+
+if(myArgs[10]=='combined') myFlux <- calcFlux(myArgs[5], datect, myArgs[6], myArgs[7], myArgs[8],sectorList = sectorList)
 
 if(myArgs[10]=='anthro'){ 
-	myFlux <- calcFlux_anthro(myArgs[5], datect, myArgs[6], myArgs[7], myArgs[8],sectorList = ifelse(myArgs[9]=='None',1:10,myArgs[9]))
+	myFlux <- calcFlux_anthro(myArgs[5], datect, myArgs[6], myArgs[7], myArgs[8],sectorList = sectorList)
 	writeNetCDF(myArgs[5], datect, myArgs[6], myFlux)
 }
 
 if(myArgs[10]=='bio'){ 
-	myFlux <- calcFlux_bio(myArgs[5], datect, myArgs[6], myArgs[7], myArgs[8],sectorList = ifelse(myArgs[9]=='None',1:10, myArgs[9]))
+	myFlux <- calcFlux_bio(myArgs[5], datect, myArgs[6], myArgs[7], myArgs[8],sectorList = sectorList)
 	writeNetCDF(myArgs[5], datect, myArgs[6], myFlux)
 }
