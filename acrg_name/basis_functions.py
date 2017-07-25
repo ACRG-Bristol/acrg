22d38f2f022605366848887e781d760e4c573e4b
Anita Ganesan <anita.ganesan@bristol.ac.uk>
2017-07-25 21:15:13 +0100

    updated basis_fuctions.py to create  new basis functions

--- acrg_name/basis_functions.py
+++ acrg_name/basis_functions.py
@@ -12,11 +12,26 @@ import xarray as xray
 import glob
 import pandas as pd
 import acrg_name as name
+import os
+from os.path import join
 
-# read in fields file to get domain
-fields_file_path = "/shared_data/air/shared/NAME/fp/"
-basis_dir = "/data/shared/NAME/basis_functions/"
-bc_basis_dir = "/data/shared/NAME/bc_basis_functions/"
+acrg_path = os.getenv("ACRG_PATH")
+data_path = os.getenv("DATA_PATH")
+
+if acrg_path is None:
+    acrg_path = os.getenv("HOME")
+    print("Default ACRG directory is assumed to be home directory. Set path in .bashrc as \
+            export ACRG_PATH=/path/to/acrg/repository/ and restart python terminal")
+if data_path is None:
+    data_path = "/data/shared/"
+    print("Default Data directory is assumed to be /data/shared/. Set path in .bashrc as \
+            export DATA_PATH=/path/to/data/directory/ and restart python terminal")
+
+# These are the default directories if no optional arguments are specified in footprints_data_merge,
+# bc_sensitivity or fp_sensitivity
+fields_file_path = join(data_path, 'NAME/fp/')
+basis_dir = join(data_path, 'NAME/basis_functions/')
+bc_basis_dir = join(data_path,'NAME/bc_basis_functions/')
 
 
 def basis_blocks(domain = "SOUTHASIA", time = "2012-01-01", blocksize = 25, basis_case = "25x25"): 
@@ -105,8 +120,9 @@ def basis_transd(domain = "SOUTHASIA", time = "2012-01-01", basis_case = "transd
                       'sub_lon': (['sub_lon'],sub_lon)})
                 
     basis_ds.to_netcdf(basis_dir + domain +"/" + basis_case + "_" + domain + "_" + str(time.year) + ".nc")
+    
 
-def basis_bc_blocks(domain = "SOUTHASIA", basis_case = "NESW", time = "2012-01-01", vertical=4):
+def basis_bc_blocks(domain = "SOUTHASIA", basis_case = "NESW", time = "2012-01-01", vertical=1):
     
 # creates uniform blocks for each direction (NESW). 
 # Each direction is split into vertical slabs with number specified by input 'vertical'
@@ -151,16 +167,62 @@ def basis_bc_blocks(domain = "SOUTHASIA", basis_case = "NESW", time = "2012-01-0
                           'lat':(['lat'], lat), \
                           'region':(['region'],regions), \
                           'time':(['time'],[time])})
+
+    basis_ds.to_netcdf(bc_basis_dir + domain +"/" + basis_case + "_" + domain + "_" + time.strftime('%m')+str(time.year) + ".nc")                
+    
+def basis_bc_uniform(domain = "SOUTHASIA", basis_case = "uniform", time = "2012-01-01"):
+    
+# creates one uniform scaling for all directions 
+
+    files = glob.glob(fields_file_path + domain + "/*")
+    
+    time = pd.to_datetime(time)
+    
+    with xray.open_dataset(files[0]) as temp:
+        fields_ds = temp.load()
+
+    if len(files) == 0:
+        print("Can't find Fields files: " + domain)
+        return None
+    
+    lat = fields_ds["lat"].values
+    lon = fields_ds["lon"].values
+    
+    regions = range(1)
+    heights = range(500,20500,1000)
+    
+    basis_grid_e = np.zeros((len(heights),len(lat),len(regions),1))
+    basis_grid_w = np.zeros((len(heights),len(lat),len(regions),1))
+    basis_grid_n = np.zeros((len(heights),len(lon),len(regions),1))
+    basis_grid_s = np.zeros((len(heights),len(lon),len(regions),1))
+    
+            
+    basis_grid_n[:,:,:,:] = 1    
+    basis_grid_e[:,:,:,:] = 1  
+    basis_grid_s[:,:,:,:] = 1
+    basis_grid_w[:,:,:,:] = 1
+        
+    basis_ds = xray.Dataset({'bc_basis_n':(['height','lon','region','time'], basis_grid_n), \
+                            'bc_basis_s':(['height','lon','region','time'], basis_grid_s), \
+                            'bc_basis_e':(['height','lat','region','time'], basis_grid_e), \
+                            'bc_basis_w':(['height','lat','region','time'], basis_grid_w)}, \
+                coords = {'height': (['height'], heights), \
+                          'lon':(['lon'], lon), \
+                          'lat':(['lat'], lat), \
+                          'region':(['region'],regions), \
+                          'time':(['time'],[time])})
                 
-    basis_ds.to_netcdf(bc_basis_dir + domain +"/" + basis_case + "_" + domain + "_" + str(time.year) + ".nc")
+    basis_ds.to_netcdf(bc_basis_dir + domain +"/" + basis_case + "_" + domain + "_" + time.strftime('%m')+str(time.year) + ".nc")
 
 def basis_bc_all_gradients(domain = "SOUTHASIA", time = "2012-01-15", species='ch4', units='ppb', basis_case='horiz-strat-grad'):
 # creates five terms for bc basis regions
-#           	      (1) offset to shift entire field up and down
+#           	     (1) offset to shift entire field up and down
 #				(2) factor to scale the entire field
 #				(3) scaling of N-S gradient
 #				(4) scaling of E-W gradient
-#				(5) scaling of stratospheric gradient 
+#				(5) scaling of stratospheric gradient
+# technically file is species specific as it calculates the stratosphere based on the vmm but one can 
+# assume/hope that the stratosphere doesn't change by species 
     
     files = glob.glob(fields_file_path + domain + "/*")
     
@@ -240,7 +302,8 @@ def basis_bc_all_gradients(domain = "SOUTHASIA", time = "2012-01-15", species='c
 
     secderiv = np.diff(vmr_all, n=2, axis=0)
     tropopause = np.argmin(secderiv[10:15,:], axis=0)+10+1 # add 1 as compromise. Because of finite differencing, levels will be decreased by 2
-    
+
+        
     stratgradient =  np.zeros(np.shape(vmr_all)) 
     
     for ii in range(2*(len(lat)+len(lon))):
@@ -307,135 +370,11 @@ def basis_bc_all_gradients(domain = "SOUTHASIA", time = "2012-01-15", species='c
                 
     basis_ds.to_netcdf(bc_basis_dir + domain +"/" + basis_case + "_" + domain + "_" + time.strftime('%m')+str(time.year) + ".nc")           
 
-def basis_bc_strat_gradient(domain = "SOUTHASIA", time = "2012-01-15", species='ch4', units='ppb', basis_case='stratgrad'):
-# creates three terms for bc basis regions
-#                (1) offset to shift entire field up and down
-#			(2) factor to scale the entire field
-#			(3) scaling of stratospheric gradient
-    
-    files = glob.glob(fields_file_path + domain + "/*")
-    
-    time = pd.to_datetime(time)
-    
-    with xray.open_dataset(files[0]) as temp:
-        fields_ds = temp.load()
-
-    if len(files) == 0:
-        print("Can't find Fields files: " + domain)
-        return None
-    
-    lat = fields_ds["lat"].values
-    lon = fields_ds["lon"].values
-
-    heights = range(500,20500,1000)
-
-    bc_ds =  name.boundary_conditions(domain, species)
-          
-    bc_ds = bc_ds.sel(time = time)
-
-    if units is 'ppb':
-        bc_ds = bc_ds*1e9
-    elif units is 'ppm':
-        bc_ds = bc_ds*1e6
-        
-    vmr_n = bc_ds["vmr_n"].values
-    vmr_e = bc_ds["vmr_e"].values
-    vmr_s = bc_ds["vmr_s"].values
-    vmr_w = bc_ds["vmr_w"].values
-    
-    #flip the east and south directions to make continuous
-    
-    vmr_e_fl = np.fliplr(vmr_e)
-    vmr_s_fl = np.fliplr(vmr_s)
-    
-    # stick together
-    
-    ind = [None]*4
-    vmr_all = np.concatenate((vmr_n, vmr_e_fl, vmr_s_fl, vmr_w), axis = 1)
-    
-    # add component that will scale entire field up and down by some amount
-    
-    scale_n = 1*np.ones(np.shape(vmr_n))
-    scale_e = 1*np.ones(np.shape(vmr_e))
-    scale_s = 1*np.ones(np.shape(vmr_s))
-    scale_w = 1*np.ones(np.shape(vmr_w))
-    scale_all = np.concatenate((scale_n, scale_e, scale_s, scale_w), axis = 1)
-    
-    ind[0] = range(len(lon))
-    ind[1] = range(max(ind[0])+1,max(ind[0])+1 + len(lat))
-    ind[2] = range(max(ind[1])+1,max(ind[1])+1 + len(lon))
-    ind[3] = range(max(ind[2])+1,max(ind[2])+1 + len(lat))
-
-    # find maximum rate of change (second derivative)
-
-    secderiv = np.diff(vmr_all, n=2, axis=0)
-    tropopause = np.argmin(secderiv[10:15,:], axis=0)+10+1 # add 1 as compromise. Because of finite differencing, levels will be decreased by 2
-    
-    stratgradient =  np.zeros(np.shape(vmr_all)) 
-    
-    for ii in range(2*(len(lat)+len(lon))):
-        stratgradient[tropopause[ii]:,ii] = -20*np.array(range(len(heights)-tropopause[ii]))      
-        
-    pcs = []
-    pcs.append(scale_all)
-    pcs.append(vmr_all)
-    pcs.append(stratgradient)
-    
-    numregions = len(pcs)
-    regions = range(numregions)
-    
-    basis_grid_n = np.zeros((len(heights),len(lon),len(regions)))
-    basis_grid_e = np.zeros((len(heights),len(lat),len(regions)))
-    basis_grid_s = np.zeros((len(heights),len(lon),len(regions)))
-    basis_grid_w = np.zeros((len(heights),len(lat),len(regions)))
-
-
-    # unravel pcs into directions
-    pc_n = []
-    pc_e = []
-    pc_s = []
-    pc_w = []
-    
-    for ii in range(numregions):
-        pc_n.append(pcs[ii][:,ind[0]])
-        pc_e.append(pcs[ii][:,ind[1]])
-        pc_s.append(pcs[ii][:,ind[2]])
-        pc_w.append(pcs[ii][:,ind[3]])
-
-    # reverse pcs for east and south to match VMRs
-    for ii in range(numregions):
-        pc_e[ii] = np.fliplr(pc_e[ii])
-        pc_s[ii] = np.fliplr(pc_s[ii])
-                
-    # create basis grids in format needed for inversion
-        
-    for ii in range(numregions):
-        basis_grid_n[:,:,ii] = pc_n[ii]/vmr_n
-        basis_grid_e[:,:,ii] = pc_e[ii]/vmr_e
-        basis_grid_s[:,:,ii] = pc_s[ii]/vmr_s
-        basis_grid_w[:,:,ii] = pc_w[ii]/vmr_w
-        
-    basis_grid_n = basis_grid_n[:,:,:,None]
-    basis_grid_e = basis_grid_e[:,:,:,None]
-    basis_grid_s = basis_grid_s[:,:,:,None]
-    basis_grid_w = basis_grid_w[:,:,:,None]
-    
-
-    basis_ds = xray.Dataset({'bc_basis_n':(['height','lon','region','time'], basis_grid_n), \
-                            'bc_basis_s':(['height','lon','region','time'], basis_grid_s), \
-                            'bc_basis_e':(['height','lat','region','time'], basis_grid_e), \
-                            'bc_basis_w':(['height','lat','region','time'], basis_grid_w)}, \
-                coords = {'height': (['height'], heights), \
-                          'lon':(['lon'], lon), \
-                          'lat':(['lat'], lat), \
-                          'region':(['region'],regions), \
-                          'time':(['time'],[time])})
-                
-    basis_ds.to_netcdf(bc_basis_dir + domain +"/" + basis_case + "_" + domain + "_" + time.strftime('%m')+str(time.year) + ".nc")           
  
                
 def basis_bc_pca(domain = "SOUTHASIA", time = "2012-01-15", species='ch4', units='ppb', basis_case='pca', numregions = 4):
 #   breaks the MOZART VMR into the first 'numregions' principal components and scales each PC
+# will be species specific
     
     files = glob.glob(fields_file_path + domain + "/*")
     
@@ -542,7 +481,7 @@ def basis_bc_pca(domain = "SOUTHASIA", time = "2012-01-15", species='ch4', units
                           'region':(['region'],regions), \
                           'time':(['time'],[time])})
                 
-    basis_ds.to_netcdf(bc_basis_dir + domain +"/" + basis_case + "_" + domain + "_" + time.strftime('%m')+str(time.year) + ".nc")           
+    basis_ds.to_netcdf(bc_basis_dir + domain +"/" + species + "_" + basis_case + "_" + domain + "_" + time.strftime('%m')+str(time.year) + ".nc")           
  
  
 def cut_array2d(array, shape):
