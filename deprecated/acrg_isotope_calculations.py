8a200984786d139dec6ab30eba8cb8693b70c523
ag12733 <ag12733@snowy.chm.bris.ac.uk>
2017-08-04 11:31:39 +0100

    moved files to deprecated

---
+++ deprecated/acrg_isotope_calculations.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 27 14:32:15 2015
+
+@author: aw12579
+"""
+
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt   
+import numpy as np
+import wea_FAAM as fa 
+#import wea_Bag
+import datetime
+import acrg_time
+import acrg_agage as agage
+import acrg_name_xray as name
+import wea_background_insitu
+import xray
+
+
+
+
+
+def read14Cdata(site,start,end):
+    if site == 'TAC':
+        data= agage.get_obs([site], "co2c14", network = ["GAUGE"], instrument = ["GAUGE-GLASS"] , start = start, end = end,height = '185m')
+    else:
+        data= agage.get_obs([site], "co2c14", network = ["GAUGE"], instrument = ["GAUGE-GLASS"] , start = start, end = end)
+    return data
+
+def read13CCO2data(site,start,end):
+    if site == 'TAC':
+        data= agage.get_obs([site], "co2c13", network = ["GAUGE"], instrument = ["GAUGE-GLASS"] , start = start, end = end,height = '185m')
+    else:
+        data= agage.get_obs([site], "co2c13", network = ["GAUGE"], instrument = ["GAUGE-GLASS"] , start = start, end = end)
+    return data
+
+def calculate14Cenhancment(site,start,end):
+    huba =read14Cdata(site,start,end)
+    #data = {site: huba[site]}
+    huba['.units']=1.
+    fp=name.footprints_data_merge(huba, domain = "EUROPE", species = "co2c14", calc_bc = False, calc_timeseries = True) # cant calucluate boundery contitions yet, throws error message 
+    return fp  
+    
+def readCO2data(site,start,end):
+    if site == 'TAC':
+        data= agage.get_obs([site], "co2", network = ["GAUGE"], instrument = ["GAUGE-GLASS"] , start = start, end = end,height = '185m')
+    else:
+        data= agage.get_obs([site], "co2", network = ["GAUGE"], instrument = ["GAUGE-GLASS"] , start = start, end = end)
+    return data    
+    
+def caclulateRH(site,start,end,permil):
+    #if site == 'TAC':
+    #    fp =name.footprints(site, start = start, end = end, domain='EUROPE', height ='185m', species = None) #takes long
+    #else:
+    #    fp =name.footprints(site, start = start, end = end, domain='EUROPE', species = None) #takes long  
+    huba =read14Cdata(site,start,end)
+    huba['.units']=1.
+    fp=name.footprints_data_merge(huba, domain = "EUROPE", species = "co2-rh", calc_bc = False, calc_timeseries = True) # cant calucluate boundery contitions yet, throws error message 
+    #flux_RH =name.flux('EUROPE', 'co2-rh')
+    #comb=name.combine_datasets(flux_RH,fp)
+    #ts=name.timeseries(comb)
+    
+    ts=fp[site].mf_mod
+    rh_ts=C14_bigdelta_to_molpermol(permil,ts,-19)
+    
+    return rh_ts
+    
+def C14_molpermol_to_bigdelta(molpermol_C14,molpermol_C12=0.0004, delta_C13=-8.8):
+    Rref=0.000000000001176
+    Delta14=((molpermol_C14/molpermol_C12)*(1-2*(25+delta_C13)/1000)/Rref-1.0)*1000.0
+    return Delta14
+    
+def C14_bigdelta_to_molpermol(Delta_C14,molpermol_C12=0.0004, delta_C13=-8.8):
+    Rref=0.000000000001176     
+    molpermol_C14=(Delta_C14/1000.0+1.0)*Rref/(1.0-(2.0*(25.0+delta_C13))/1000.0)*molpermol_C12 
+    return molpermol_C14
+    
+def C13_molpermol_to_smalldelta(molpermol_C13,molpermol_C12=0.0004):
+    delta13= (molpermol_C13/(molpermol_C12)/0.0112372-1.0)*1000.0
+    return delta13
+    
+def C13_smalldelta_to_molpermol(delta_C13,molpermol_C12=0.0004):
+    molpermol_C13= (delta_C13/1000.0+1.0)*molpermol_C12*0.0112372
+    return molpermol_C13
+
+
---
+++ deprecated/acrg_noaa.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 13 16:43:34 2014
+
+Extract NOAA observations stored in CF convention format
+
+Examples:
+
+Get all site names from stored filenames:
+    
+    import acrg_noaa as noaa
+    noaa.sites("CH4")
+        ['ABP',
+         'ALT',
+         'AMS',
+         ...]
+
+Read monthly NOAA methane data for 2005 at Mace Head:
+
+    import acrg_noaa as noaa
+    data=noaa.read("MHD", "CH4", startYear=2005, endYear=2006, monthly=True)
+    print data.time
+        [datetime.datetime(2005, 1, 31, 0, 0),
+         datetime.datetime(2005, 2, 28, 0, 0),
+         datetime.datetime(2005, 3, 31, 0, 0),
+         ...]
+
+    print data.mf
+        array([ 1847.11364746,  1850.75964355,  1843.13830566,  1983.51013184,
+                1855.42993164,  1864.32507324,  1831.22009277,  1815.51501465,
+                1836.64611816,  1873.51403809,  1869.26098633,  1856.56835938], dtype=float32)
+
+sitelocs returns the FIRST lat/lon listed in each NOAA flask file.
+
+      
+@author: Matt Rigby
+"""
+
+import numpy as np
+import acrg_cf as cf
+import glob
+import pandas
+import datetime as dt
+
+def directory(species):
+    
+    baseDirectory='/data/shared/GAUGE/'
+    speciesDirectory={"CH4":"CH4/NOAA_flask/processed/", \
+                        "CO2":"CO2/NOAA_flask/processed/"}
+    return baseDirectory + speciesDirectory[species.upper()]
+
+def filename(site, species):
+
+    dirname=directory(species)
+    filenames=glob.glob(dirname + "*")
+    filename=[f for f in filenames if ('_' + site.upper() )in f.upper()]
+    
+    if len(filename) > 1:
+        print "More than one matching filename " + site.upper()
+        return 0
+    if len(filename) == 0:
+        print "No matching filename " + site.upper()
+        return 0
+
+    return filename[0]
+
+def varname(species):
+    
+    varnames={"CH4":"ch4", \
+                "CO2": "co2"}
+    varname=varnames[species.upper()]
+    return varname
+
+def sites(species):
+    
+    filenames=glob.glob(directory(species) + '*')
+    site=[]
+    for f in filenames:
+        s=f[f.rfind('/')+1:]
+        s=s[s.find('_')+1:]
+        site.append(s[0:s.find('_')].upper())
+    
+    site.sort()
+
+    return site
+
+class sitelocs:
+    def __init__(self, species):
+    
+        sitenames = sites(species)    
+    
+        filenames=glob.glob(directory(species) + '*')
+    
+        lat=np.empty(np.shape(sitenames))
+        lon=np.empty(np.shape(sitenames))
+        alt=np.empty(np.shape(sitenames))
+        
+        for f in np.arange(len(filenames)):
+            data_f = read(sitenames[f], species)
+            lat[f] = data_f.lat[0]
+            lon[f] = data_f.lon[0]
+            alt[f] = data_f.alt[0]
+
+        self.lat = lat
+        self.lon = lon
+        self.alt = alt
+
+
+class read:
+    def __init__(self, site, species, \
+        startYear=2000, endYear=2014, monthly=True):
+        
+        #Read (CF-compliant) netCDF file
+        fileOut = cf.read(filename(site, species), varname(species))
+        
+        time=fileOut.time
+        mf=fileOut.mf
+        
+        #If there isn't an element at the start or end of the desired
+        # time period, add a null mole fraction. This helps create a 
+        # regular array if resampling
+        if monthly:
+            if min(time) > dt.datetime(startYear, 1, 31):
+                time.append(dt.datetime(startYear, 1, 1))
+                
+                mf=np.append(mf, np.array(float('nan')))
+            if max(time) < dt.datetime(endYear-1, 12, 31):
+                time.append(dt.datetime(endYear-1, 12, 31))
+                mf=np.append(mf, np.array(float('nan')))
+
+        #Create Pandas time series
+        ts=pandas.Series(mf, index=time)
+
+        #Extract monthly means, if requested
+        if monthly:
+            ts=ts.resample('M', how='mean')
+
+        ts=ts.truncate(dt.datetime(startYear, 1, 1), \
+            dt.datetime(endYear, 1, 1))
+        
+        self.time=(ts.index.to_pydatetime()).tolist()
+        self.mf=np.array(ts[:])
+        self.lon=fileOut.lon
+        self.lat=fileOut.lat
+        self.alt=fileOut.alt
+        
