eb201220b51ac2ab9f41497538d89acc840e9634
Anita Ganesan <ag12733@bristol.ac.uk>
2019-02-20 16:27:09 +0000

    updated acrg_time files to python 3
    Former-commit-id: e4f770a9e1864d63aa5fa9dccf0b6126d8520e0b

--- acrg_time/__init__.py
+++ acrg_time/__init__.py
@@ -4,8 +4,9 @@ Created on Fri Nov 21 11:03:33 2014
 
 @author: chxmr
 """
+from __future__ import absolute_import
 
-import convert
-from kz_filter import kz_filter
+from . import convert
+from .kz_filter import kz_filter
 #from smooth import smooth
 #import baseline
