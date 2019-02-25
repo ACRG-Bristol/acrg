77ee0e73d59e0b127edeef1491f9c8529f98a57a
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2019-02-25 17:09:03 +0000

    Fixed bug in gosat.py to resolve test_gosat.py::test_pressure_match_offset. Issue was related to Bug 1463 in xarray where a deep copy of a dataset was not copying coordinates.
    Former-commit-id: 65de160642513ff581d7a1a77aff3329d7b633f5

--- acrg_satellite/gosat.py
+++ acrg_satellite/gosat.py
@@ -1636,9 +1636,11 @@ def name_pressure_match(ds,pressure_domain,columns=["latitude","longitude","time
             match = pressure_NAME.sel(method="nearest",**{columns[0]:la,columns[1]:lo,columns[2]:t})
         else:
             # Assumes offset is +1 day because we expect NAME run for e.g. 2012-01-01 to end with datetime 2012-02-01 00:00:00.
-            max_offset_days = np.timedelta64((t - end_datetime),'D').astype(int)+1
+            max_offset_days = np.timedelta64((t - end_datetime),'D') + np.timedelta64(1,"D")
             pressure_NAME_offset = pressure_NAME.copy(deep=True)
-            pressure_NAME_offset[columns[2]] += np.timedelta64(max_offset_days,'D') # Create copy of dataset with date offset to date we're looking for.
+            # Have to add extra line to explicitly copy the time coordinates because of an xarray bug - https://github.com/pydata/xarray/issues/1463
+            pressure_NAME_offset.coords[columns[2]].data = np.copy(pressure_NAME_offset.coords[columns[2]].data)
+            pressure_NAME_offset[columns[2]].values += max_offset_days # Create copy of dataset with date offset to date we're looking for.
             #day_tolerance = tolerance[:1] + [np.timedelta64(1,'D')]
             match = pressure_NAME_offset.sel(method="nearest",**{columns[0]:la,columns[1]:lo,columns[2]:t})
         matched_pressure_NAME[i] = match[p_column].values
