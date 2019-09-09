384a67da170ffe0b1c30e30198c8f519a94461c1
Matt Rigby <matt.rigby@bristol.ac.uk>
2019-09-09 12:59:25 +0100

    Added network key to arg_obs.utils.attributes site into, so hopefully it will now add site info to netcdf files
    Former-commit-id: 2d34dfd672d3bc5a47bca03d12cf3ea88980d43b

--- acrg_obs/utils.py
+++ acrg_obs/utils.py
@@ -81,7 +81,7 @@ species_translator = {"CO2": ["co2", "carbon_dioxide"],
                       "PFC-318": ["c4f8", "cyclooctafluorobutane"],
                       "F-113": ["cfc113", "cfc113"],
                       "H2_PDD": ["h2", "hydrogen"],
-                      "NE_PDD": ["Ne", "neon"],                      
+                      "NE_PDD": ["Ne", "neon"],
                       "DO2N2": ["do2n2", "ratio_of_oxygen_to_nitrogen"],
                       "DCH4C13": ["dch4c13", "delta_ch4_c13"],
                       "DCH4D": ["dch4d", "delta_ch4_d"],
@@ -92,28 +92,32 @@ species_translator = {"CO2": ["co2", "carbon_dioxide"],
 
 
 
-def site_info_attributes(site):
+def site_info_attributes(site, network):
 
     # Read site info file
     site_info_file = join(acrg_path, "acrg_site_info.json")
     with open(site_info_file) as sf:
         site_params = json.load(sf)
-        
+
     attributes = {}
     attributes_list = {"longitude": "station_longitude",
                        "latitude": "station_latitude",
                        "long_name": "station_long_name",
                        "height_station_masl": "station_height_masl"}
 
+    if network is None:
+        net = site_params[site].keys()[0]
+
     if site in list(site_params.keys()):
         for at in list(attributes_list.keys()):
-            if at in list(site_params[site].keys()):
-                attributes[attributes_list[at]] = site_params[site][at]
-        return attributes
+            if at in list(site_params[site][net].keys()):
+                attributes[attributes_list[at]] = site_params[site][net][at]
+            return attributes
     else:
         return None
 
 def attributes(ds, species, site,
+               network = None,
                global_attributes = None,
                units = None,
                scale = None,
@@ -125,34 +129,34 @@ def attributes(ds, species, site,
     """
     Format attributes for netCDF file
     Attributes of xarray DataSet are modified, and variable names are changed
-    
+
     If the species is a standard mole fraction then either:
         - species name will used in lower case in the file and variable names
             but with any hyphens taken out
         - name will be changed according to the species_translator dictionary
-    
+
     If the species is isotopic data or a non-standard variable (e.g. APO):
         - Isotopes species names should begin with a "D"
             (Annoyingly, the code currently picks up "Desflurane" too. I've
-             fixed this for now, but if we get a lot of other "D" species, we 
+             fixed this for now, but if we get a lot of other "D" species, we
              should make this better)
         - I suggest naming for isotopologues should be d<species><isotope>, e.g.
             dCH4C13, or dCO2C14
         - Any non-standard variables should be listed in the species_translator
             dictionary
-    
+
     Args:
         ds (xarray dataset): Should contain variables such as "ch4", "ch4 repeatability".
             Must have a "time" dimension.
         species (string): Species name. e.g. "CH4", "HFC-134a", "dCH4C13"
         site (string): Three-letter site code
-        
-        global_attribuates (dict, optional): Dictionary containing any info you want to 
+
+        global_attribuates (dict, optional): Dictionary containing any info you want to
             add to the file header (e.g. {"Contact": "Matt Rigby"})
         units (string, optional): This routine will try to guess the units
             unless this is specified. Options are in units_interpret
         scale (string, optional): Calibration scale for file header.
-        sampling_period (int, optional): Number of seconds for which air 
+        sampling_period (int, optional): Number of seconds for which air
             sample is taken. Only for time variable attribute
         date_range (list of two strings, optional): Start and end date for output
             If you only want an end date, just put a very early start date
@@ -175,19 +179,19 @@ def attributes(ds, species, site,
             else:
                 # Rename species to be lower case and without hyphens
                 species_out = species.lower().replace("-", "")
-                
+
             rename_dict[key] = key.replace(species.lower(), species_out)
     ds = ds.rename(rename_dict)
-            
+
     # Check if these was a variable with the species name in it
     try:
       species_out
     except NameError:
       print("ERROR: Can't find species %s in column names %s" %(species, list(ds.keys())))
-      
+
     # Global attributes
     #############################################
-    
+
     if global_attributes is None:
         global_attributes = {}
         for key in global_attributes_default:
@@ -200,14 +204,14 @@ def attributes(ds, species, site,
     global_attributes["File created"] = str(dt.now())
 
     # Add user
-    global_attributes["Processed by"] = "%s@bristol.ac.uk" % getpass.getuser()    
+    global_attributes["Processed by"] = "%s@bristol.ac.uk" % getpass.getuser()
 
 
     for key, values in global_attributes.items():
         ds.attrs[key] = values
 
     # Add some site attributes
-    global_attributes_site = site_info_attributes(site.upper())
+    global_attributes_site = site_info_attributes(site.upper(), network)
     if global_attributes_site is not None:
         for key, values in global_attributes_site.items():
             ds.attrs[key] = values
@@ -258,7 +262,7 @@ def attributes(ds, species, site,
                 if species.upper() in list(unit_species_long.keys()):
                     ds[key].attrs["units_description"] = unit_species_long[species.upper()]
 
-            # Add to list of ancilliary variables                    
+            # Add to list of ancilliary variables
             if key != species_out:
                 ancillary_variables += key + ", "
 
@@ -326,24 +330,24 @@ def output_filename(output_directory,
                     inlet = None,
                     version = None):
     """
-    Create an output filename in the format 
+    Create an output filename in the format
     output_directory/site/network-instrument_site_YYYYMMDD_species[-inlet]-version.nc
-    
+
     Also creates a site directory in output_directory, if one doesn't exist
-    
+
     Args:
-        
+
         inlet (string, optional): Label for inlet. If none supplied, assumes that there
             is only one inlet, and no inlet info is added to file name
         version (string, optional): Version number for file. If none supplied, will
             use YYYYMMDD for day processed
-    
+
     """
-    
+
     # Check if directory exists. If not, create it
     if not os.path.exists(join(output_directory, site)):
         os.makedirs(join(output_directory, site))
-    
+
     # Create suffix. If inlet is specified, append to species name
     suffix = species
     if inlet is not None:
@@ -352,7 +356,7 @@ def output_filename(output_directory,
         suffix += "-" + version
     else:
         suffix += "-" + time.strftime("%Y%m%d")
-    
+
     # Return output filename
     return join(output_directory,
                 "%s/%s-%s_%s_%04i%02i%02i_%s.nc" %(site,
@@ -371,14 +375,14 @@ def cleanup(site,
     Archive old versions of files in an archive folder within each site folder.
     Will keep the maxiumum version number for each NETWORK-INSTRUMENT, and move
     older ones to a zip file.
-    
+
     Args:
         site (string): Measurement site
-        
+
         version (string): Specify a version to archive. If None, will archive older versions
     '''
-    
-    
+
+
     import zipfile
 
     # Directories and files
@@ -388,10 +392,10 @@ def cleanup(site,
 
     print("Checking for files to archive in %s ..." % data_directory)
 
-    # Work out instrument (NETWORK-INSTRUMENT) and suffix from filename    
+    # Work out instrument (NETWORK-INSTRUMENT) and suffix from filename
     instrument = []
     suffix = []
-    
+
     for f in files:
         f_data = os.path.split(f)[1].split(".")[0].split("_")
         instrument.append(f_data[0])
@@ -400,14 +404,14 @@ def cleanup(site,
     # Identify unique instruments
     unique_instruments = set(instrument)
 
-    # Cycle through each NETWORK-INSTRUMENT        
+    # Cycle through each NETWORK-INSTRUMENT
     for i in unique_instruments:
 
         # Find versions
         versions = [s.split("-")[-1] for (s, inst) in zip(suffix, instrument) if inst == i]
         unique_versions = set(versions)
         latest_version = max(unique_versions)
-        
+
         # By default, archive everything apart from the latest version
         archive_versions = [v for v in unique_versions if v != latest_version]
 
@@ -426,7 +430,7 @@ def cleanup(site,
         for v in archive_versions:
 
             print("... archiving %s files, version %s" % (i, v) )
-            
+
             # Check if archive directory exists, and create if not
             if not os.path.exists(archive_directory):
                 os.makedirs(archive_directory)
@@ -444,9 +448,8 @@ def cleanup(site,
                 zipf.write(f, os.path.basename(f))
                 os.remove(f)
             zipf.close()
-            
+
 def cleanup_all():
     site_list = [site_dir for site_dir in os.listdir(obs_directory) if os.path.isdir(os.path.join(obs_directory,site_dir))]
     for site in site_list:
         cleanup(site)
-        
