Fixed checkIndexes bug and added 1 second precision test
Former-commit-id: 070e8677e013554bea52832a23d060d272e787bc

acrg_name/name.py
@@ -666,8 +666,14 @@ def indexesMatch(dsa, dsb):
         #first check lengths are the same to avoid error in second check
         if not len(dsa.indexes[index])==len(dsb.indexes[index]):
             return False
-        #check number of values that are not close (testing for equality with floating point sucks)
-        if not np.sum(~np.isclose(dsa.indexes[index].values.astype(float),dsb.indexes[index].values.astype(float) ))==0:
+        
+        #check number of values that are not close (testing for equality with floating point)
+        if index == "time":
+            #for time iverride the default to have ~ second precision
+            rtol = 1e-10
+        else:
+            rtol = 1e-5
+        if not np.sum(~np.isclose(dsa.indexes[index].values.astype(float),dsb.indexes[index].values.astype(float), rtol=rtol ))==0:
             return False
         
     return True

tests/test_name.py
@@ -312,6 +312,15 @@ def dsb():
                           coords={'time':times})
     return dsb
 
+@pytest.fixture()
+def dsc():
+    times = pd.date_range("2018-01-01", "2018-02-01", freq='4H')
+    vals = np.ones_like(times, dtype='float')
+    dsc = xray.Dataset({'vals_c': (['time'], vals)},
+                          coords={'time':times})
+    dsc.time.values[1] = dsc.time.values[1] + pd.Timedelta("1 second")
+    return dsc
+
 def test_combine_datasets(dsa, dsb, method = "nearest", tolerance = None):
     """
     Merge two datasets.   
@@ -345,13 +354,13 @@ def test_align_datasets(dsa, dsb):
         
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
     
-def test_indexesMatch(dsa, dsb):
+def test_indexesMatch(dsa, dsc):
     dsa2 = dsa.copy() * 2.
     
     errors = []
     if not name.indexesMatch(dsa, dsa2) == True:
         errors.append("Same indexes not seen as the same")
-    if not name.indexesMatch(dsa, dsb) == False:
+    if not name.indexesMatch(dsa, dsc) == False:
         errors.append("Different indexes not seen as different")
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
 