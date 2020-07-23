f28df3ad1cbc28c88cea89a2f530927b71b374df
mi19881 <mi19881@bp1-login01.data.bp.acrc.priv>
2020-07-23 10:54:26 +0100

    Changed basis_functions.py to use acrg_config.paths
    Former-commit-id: 88f52b1175f26fef49881a141ac95117aed37cb1

--- acrg_name/basis_functions.py
+++ acrg_name/basis_functions.py
@@ -19,12 +19,18 @@ import pandas as pd
 import acrg_name as name
 import os
 from os.path import join
+import sys
 import scipy.optimize
 import getpass
 import uuid
 
-acrg_path = os.getenv("ACRG_PATH")
-data_path = os.getenv("DATA_PATH")
+if sys.version_info[0] == 2: # If major python version is 2, can't use paths module
+    acrg_path = os.getenv("ACRG_PATH")
+    data_path = os.getenv("DATA_PATH") 
+else:
+    from acrg_config.paths import paths
+    acrg_path = paths.acrg
+    acrg_path = paths.data
 
 if acrg_path is None:
     acrg_path = os.getenv("HOME")
