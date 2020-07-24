688d2d214f921a63598ee3fb09a3281a5b274b94
mi19881 <mi19881@bp1-login01.data.bp.acrc.priv>
2020-07-24 11:34:15 +0100

    Update on using path
    Former-commit-id: cee870ee416706f0ff6a3e186a60df051c31b676

--- tests/test_flux.py
+++ tests/test_flux.py
@@ -15,10 +15,10 @@ if sys.version_info[0] == 2: # If major python version is 2, can't use paths mod
     data_path = os.getenv("DATA_PATH") 
 else:
     from acrg_config.paths import paths
-    data_path = paths.data
+    data_path = str(paths.data)
 
 def test_write(tmpdir):
-    with xr.open_dataset(str(data_path)+'/LPDM/emissions/EUROPE/n2o-ocean_EUROPE_2009.nc') as load:
+    with xr.open_dataset(data_path+'/LPDM/emissions/EUROPE/n2o-ocean_EUROPE_2009.nc') as load:
         ds = load.load()
     del ds.attrs['author']
     del ds.attrs['date_created']
