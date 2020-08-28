613f305d6a284a413d570b35b488c392ec0a1201
Matt Rigby <matt.rigby@bristol.ac.uk>
2020-08-28 06:14:04 +0100

    Added pyfakefs import to test_name, and tidied up some unnecessary import statements
    Former-commit-id: 01068371fd1bac7205e40479b1b10286c3798ff6

--- tests/test_name.py
+++ tests/test_name.py
@@ -23,10 +23,6 @@ To run all tests except those labelled 'long' use the syntax
 
 @author: rt17603
 """
-from __future__ import division
-
-from builtins import range
-from past.utils import old_div
 import pytest
 import os
 import sys
@@ -34,19 +30,13 @@ import glob
 import numpy as np
 import xarray as xray
 import pandas as pd
-
 import acrg_name.name as name
-#import acrg_agage as agage
 import acrg_obs.read as read
+import pyfakefs
+from acrg_config.paths import paths
 
-#%%
 
-if sys.version_info[0] == 2: # If major python version is 2, can't use paths module
-    acrg_path = os.getenv("ACRG_PATH") 
-else:
-    from acrg_config.paths import paths
-    acrg_path = paths.acrg
-
+acrg_path = paths.acrg
 
 @pytest.fixture(scope="module")
 def fp_directory():
