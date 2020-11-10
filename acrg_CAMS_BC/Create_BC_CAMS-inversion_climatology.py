363d5311d47fa5c53202c7c74c1a0386b865423d
Anita Ganesan <ag12733@bp1-login02.data.bp.acrc.priv>
2020-11-10 14:01:24 +0000

    new code for generating CAMS BCs
    Former-commit-id: 93225f571346ca717834a6e22be422f9f4c04dab

---
+++ acrg_CAMS_BC/Create_BC_CAMS-inversion_climatology.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 11:00:20 2019
+
+@author: rt17603
+"""
+
+import cams_inversion
+import climatology
+import os
+import pandas as pd
+import numpy as np
+import xarray as xr
+from acrg_countrymask import domain_volume
+
+data_path = os.getenv("DATA_PATH")
+cams_directory = os.path.join(data_path,"ECMWF_CAMS/CAMS_inversion/")
+
+def add_full_time(ds_seasonal,start,end):
+    '''
+    Expand out climatology dataset to include full date range from start to end.
+    Expect input to be a climatology dataset with dimension of "month". This dimension will be expanded
+    and replaced with the "time" dimension.
+    '''
+    
+    date_range = []
+    start_year = int(start.split("-")[0])
+    end_year = int(end.split("-")[0])
+    
+    s = start
+    year = start_year
+    while year < end_year+1:
+        if year == end_year:
+            e = end
+        else:
+            e = str(year+1)+"-01-01"         
+        dr = np.arange(s,e,dtype="datetime64[M]").astype("datetime64[ns]")
+        if len(dr) > 0:
+            date_range.append(dr)
+        s = e
+        year+=1
+    
+    print("date_range",date_range)
+    
+    for i,dr in enumerate(date_range):
+        
+        ds_copy = ds_seasonal.copy()
+
+        print("dr",dr)
+
+        months = pd.DatetimeIndex(dr).month
+        print("months",months)
+        
+        ds_copy = ds_copy.sel(**{"month":months})
+
+        ds_copy = ds_copy.assign(**{"time":("month",dr)})
+        ds_copy = ds_copy.swap_dims({"month":"time"})
+        ds_copy = ds_copy.drop("month")
+        
+        if i == 0:
+            ds = ds_copy
+        else:
+            ds = xr.concat([ds,ds_copy],dim="time")
+
+    return ds
+
+def makeCAMSClimatologyBC(domain,st_date,st_end,
+                          clim_start=None,clim_end=None,
+                          outdir=None,cams_directory=cams_directory):
+
+    if clim_start is None and clim_end is None:
+        clim_start = st_date
+        clim_end = st_end
+    
+    species="ch4"
+
+    fp_lat,fp_lon,fp_height = domain_volume(domain)
+    
+    if outdir is None:
+        outdir = os.path.join(data_path,"LPDM/bc/{}/".format(domain))
+    
+    cams_ds = cams_inversion.readCAMSInversion(clim_start,clim_end)
+    cams_seasonal = climatology.seasonal_cycle(cams_ds)    
+    cams_bc = add_full_time(cams_seasonal,start=output_start,end=output_end)
+
+    date_range = np.arange(st_date,st_end,dtype="datetime64[M]")
+    date_range = [np.datetime_as_string(date)+"-01" for date in date_range]
+    date_range += [st_end]
+    
+    print(date_range)
+    print(st_end)
+
+    for start,end in zip(date_range[:-1],date_range[1:]):
+        
+        # Check if file exists so a current file doesn't get overwritten
+        out_filename = cams_inversion.bc_filename(domain,species,start)
+
+        if os.path.isfile(os.path.join(outdir,out_filename)):
+            print('Boundary condition file {} already exists.'.format(out_filename))
+            print('Delete old one first to replace it.')
+            continue
+        
+        ds = cams_bc.sel(**{"time":slice(start,end)})
+
+        cams_inversion.create_CAMS_BC(ds,fp_lat,fp_lon,fp_height,start,species=species,domain=domain,
+                                      outdir=outdir,cams_directory=cams_directory)
+
+if __name__=="__main__":
+
+    clim_start = "2010-01-01"
+    clim_end = "2019-01-01"
+    
+    output_start = "2019-01-01"
+    output_end = "2020-01-01"
+
+    domain = "SOUTHAFRICA"
+
+    outdir = os.path.join(data_path)
+
+    cams_bc = makeCAMSClimatologyBC(domain,output_start,output_end,clim_start,clim_end,outdir=outdir)
+    
---
+++ acrg_CAMS_BC/cams_inversion.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 11:05:12 2019
+
+@author: rt17603
+"""
+
+import os
+import numpy as np
+import xarray as xr
+from datetime import datetime as dt
+import acrg_name.name as name
+from acrg_satellite.gosat import extract_files
+import acrg_CAMS_BC as CAMS_BC
+
+data_path = os.getenv("DATA_PATH")
+cams_directory = os.path.join(data_path,"ECMWF_CAMS/CAMS_inversion/")
+
+def readCAMSInversion(start,end,cams_directory=cams_directory):
+    
+    species="ch4"
+
+    search_str = "cams73_latest_{species}*.nc".format(species=species)
+    
+    files = extract_files(cams_directory,search_str,start=start,end=end,day=False)
+    ds_list = [name.open_ds(file) for file in files]
+    ds = xr.concat(ds_list,dim="time")
+    
+    ds = ds.rename({"CH4":species})
+    
+    return ds
+
+def convertCAMSaltitude(ds):
+    
+    hlevels = ds.hlevel.values
+    h_min = hlevels[0]
+    h_max = hlevels[-1]
+
+    low_alt = ds["altitude"].sel(**{"hlevel":slice(h_min,h_max-1)})
+    high_alt = ds["altitude"].sel(**{"hlevel":slice(h_min+1,h_max)})
+    
+    altitude_diff = high_alt.values - low_alt.values
+    z = low_alt + altitude_diff/2.
+    
+    z_dims = tuple([dim if dim!="hlevel" else "level" for dim in ds["altitude"].dims])
+    
+    ds = ds.assign(**{"z":(z_dims,z)})
+    ds["z"] = ds["z"].transpose(*("time","level","latitude","longitude"))
+    
+    return ds
+
+def bc_filename(domain,species,st_date,climatology=False):
+    if climatology:
+        clim_str = "_climatology"
+    else:
+        clim_str = ""
+    return "{species}_{domain}_{date}{clim}.nc".format(species=species.lower(),domain=domain,date=dt.strptime(st_date, '%Y-%m-%d').strftime('%Y%m'),clim=clim_str)
+
+
+def create_CAMS_BC(ds,fp_lat,fp_lon,fp_height,date,domain,species="ch4",outdir=None,cams_directory=cams_directory):
+
+    lat_grid=np.mean(ds["latitude"][1:]-ds["latitude"][:-1])
+    lon_grid=np.mean(ds["longitude"][1:]-ds["longitude"][:-1])
+    gridsize="{} x {}".format(lat_grid,lon_grid)
+    
+    ds = convertCAMSaltitude(ds)
+    ds = ds.mean('time')
+    return ds
+    ds[species].values *= 1e-9 
+    
+    #Select the gridcells closest to the edges of the  domain and make sure outside of fp
+    lat_n =(np.abs(ds.coords['latitude'].values - max(fp_lat))).argmin()
+    if ds.coords['latitude'].values[lat_n] < np.max(fp_lat) and lat_n != 0:
+        lat_n -= 1
+    lat_s = (np.abs(ds.coords['latitude'].values - min(fp_lat))).argmin()
+    if ds.coords['latitude'].values[lat_s] > np.min(fp_lat) and lat_s != (len(ds.coords['latitude'].values)-1):
+        lat_s += 1
+    lon_e =  (np.abs(ds.coords['longitude'].values - max(fp_lon))).argmin()
+    if ds.coords['longitude'].values[lon_e] < max(fp_lon) and lon_e != (len(ds.coords['longitude'].values)-1):
+        lon_e += 1
+    lon_w =  (np.abs(ds.coords['longitude'].values - min(fp_lon))).argmin()
+    if ds.coords['longitude'].values[lon_w] > min(fp_lon) and lon_w != 0:
+        lon_e -= 1
+    
+    print("hello1?")
+    print("ds, z",ds["z"].dims)
+    print("ds, ch4",ds["ch4"].dims)
+    
+    #Cut to these and then interpolate
+    north = ds.sel(latitude = ds.coords['latitude'][lat_n],
+                   longitude = slice(ds.coords['longitude'][lon_w],ds.coords['longitude'][lon_e])).drop(['latitude'])
+    south = ds.sel(latitude = ds.coords['latitude'][lat_s],
+                   longitude = slice(ds.coords['longitude'][lon_w],ds.coords['longitude'][lon_e])).drop(['latitude'])
+    east = ds.sel(longitude = ds.coords['longitude'][lon_e],
+                  latitude = slice(ds.coords['latitude'][lat_s],ds.coords['latitude'][lat_n])).drop(['longitude'])
+    west = ds.sel(longitude = ds.coords['longitude'][lon_w],
+                  latitude = slice(ds.coords['latitude'][lat_s],ds.coords['latitude'][lat_n])).drop(['longitude'])
+    
+    #print("z",north["z"].dims)
+    #print("ch4",north["ch4"].dims)
+    #print(north.dims["level"],north.dims["longitude"])
+    
+    #import pdb
+    #pdb.set_trace()
+    
+    #vmr_n = CAMS_BC.interplonlat(CAMS_BC.interpheight(north, fp_height, species, reverse=False, lonorlat='longitude'), fp_lon, species, lonorlat='longitude').rename({species : 'vmr_n'})   
+    #vmr_s = CAMS_BC.interplonlat(CAMS_BC.interpheight(south, fp_height, species, reverse=False, lonorlat='longitude'), fp_lon, species, lonorlat='longitude').rename({species : 'vmr_s'}) 
+    #vmr_e = CAMS_BC.interplonlat(CAMS_BC.interpheight(east, fp_height, species, reverse=False, lonorlat='latitude'), fp_lat, species, reverse=False, lonorlat='latitude').rename({species : 'vmr_e'}) 
+    #vmr_w = CAMS_BC.interplonlat(CAMS_BC.interpheight(west, fp_height, species, reverse=False, lonorlat='latitude'), fp_lat, species, reverse=False, lonorlat='latitude').rename({species : 'vmr_w'})      
+
+    vmr_n = CAMS_BC.interplonlat(CAMS_BC.interpheight(north, fp_height, species, lonorlat='longitude'), fp_lon, species, lonorlat='longitude').rename({species : 'vmr_n'})   
+    vmr_s = CAMS_BC.interplonlat(CAMS_BC.interpheight(south, fp_height, species, lonorlat='longitude'), fp_lon, species, lonorlat='longitude').rename({species : 'vmr_s'}) 
+    vmr_e = CAMS_BC.interplonlat(CAMS_BC.interpheight(east, fp_height, species, lonorlat='latitude'), fp_lat, species, lonorlat='latitude').rename({species : 'vmr_e'}) 
+    vmr_w = CAMS_BC.interplonlat(CAMS_BC.interpheight(west, fp_height, species, lonorlat='latitude'), fp_lat, species, lonorlat='latitude').rename({species : 'vmr_w'})      
+
+    print("hello?")
+    CAMS_BC.write_CAMS_BC_tonetcdf(vmr_n, vmr_e, vmr_s, vmr_w, date, species, domain, outdir, gridsize)  
