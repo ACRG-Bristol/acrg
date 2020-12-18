164e3d2b4b0c0f3f4cddd5218b8d8e99de8d10d7
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2020-12-18 17:15:33 +0000

    Moving towards a config file input for run_tropomi_process but not implemented yet
    Former-commit-id: d03ceab5774d86726856c8c82d83b2785f322517

--- acrg_satellite/run_tropomi_process.py
+++ acrg_satellite/run_tropomi_process.py
@@ -12,14 +12,48 @@ Created on Fri Nov 13 17:33:47 2020
 #import sys
 #import argparse
 
-import numpy as np
+import os
+
 import acrg_satellite.tropomi as tropomi
 from pathlib import Path
+import argparse
+
+try:
+    from acrg_config.paths import paths
+except ImportError:
+    acrg_path = os.getenv("ACRG_PATH")
+else:
+    acrg_path = paths.acrg
+
+
 
 if __name__=="__main__":
     
-    start_date = "2019-07-11"
-    end_date = "2019-07-15"
+    config_file = os.path.join(acrg_path,"acrg_satellite/tropomi_process.ini")
+    
+    parser = argparse.ArgumentParser(description='Running tropomi process script')
+
+    parser.add_argument("start", help="Start date string yyyy-mm-dd",type=str,nargs="?")
+    parser.add_argument("end", help="End date sting yyyy-mm-dd",type=str,nargs="?")
+    parser.add_argument("-c","--config",help="Configuration filename",default=config_file)
+
+    args = parser.parse_args()
+    
+    start_date = args.start
+    end_date = args.end
+    config_file = args.config or args.config_file
+    
+    # tropomi_param = gosat_config.gosat_param(config_file)
+
+#    if start_date is not None:
+#      tropomi_param["start_date"] = start_date
+#    if end_date is not None:
+#      tropomi_param["end_date"] = end_date
+
+
+    ####### TO BE REMOVED ONCE CONFIG FILE IS IN PLACE #####
+#    start_date = "2019-07-19"
+#    end_date = "2019-07-22"
     
     area="GOSAT-BRAZIL"
 
@@ -36,14 +70,17 @@ if __name__=="__main__":
         lon_min = -76.0 # -75.98444
         lon_max = -32.7 # -32.79305
     
-    dlat = 0.1
-    dlon = 0.1
-    
+#    dlat = 0.234
+#    dlon = 0.352
+    dlat = 0.10
+    dlon = 0.10
+
     lat_bounds = [lat_min,lat_max]
     lon_bounds = [lon_min,lon_max]
     coord_bin = [dlat,dlon]
     
-    time_increment = "1s" # e.g. None, "10s", "30min" (always < 1 day)
+#    time_increment = "1s" # e.g. None, "10s", "30min" (always < 1 day)
+    time_increment = "1min"
     #time_increment = "10s" # e.g. None, "10s", "30min" (always < 1 day)
     #time_increment = "30min" # e.g. None, "10s", "30min" (always < 1 day)
     #time_increment = "1h" # e.g. None, "10s", "30min" (always < 1 day)
@@ -54,19 +91,27 @@ if __name__=="__main__":
     
     write_name = True
     #name_directory = Path("/home/rt17603/work/NAME_files/test")
-    name_directory = Path("/home/rt17603/work/NAME_files/")
+    #name_directory = Path("/home/rt17603/work/NAME_files/")
+#    name_directory = Path("/home/rt17603/work/NAME_files/03_bin")
+#    name_directory = Path("/home/rt17603/work/NAME_files/05_bin")
+#    name_directory = Path("/home/rt17603/work/NAME_files/035_1min_bin")
+    name_directory = Path("/home/rt17603/work/NAME_files/010_1min_bin")
     use_name_pressure = True
     pressure_domain = "SOUTHAMERICA"
     pressure_max_days = 1
     
     #output_directory = Path("/home/rt17603/work/obs/test")
-    output_directory = Path("/home/rt17603/work/obs/")
+#    output_directory = Path("/home/rt17603/work/obs/")
+    #output_directory = Path("/home/rt17603/work/obs/03_bin")
+#    output_directory = Path("/home/rt17603/work/obs/05_bin")
+#    output_directory = Path("/home/rt17603/work/obs/035_1min_bin")
+    output_directory = Path("/home/rt17603/work/obs/010_1min_bin")
     
     #max_points = None # Maximum number of points per NAME file
     max_points = 50 # Maximum number of points per NAME file
     
-    ## ** RERUN WITH THIS **
     regrid_method = "conservative_normed"
+
     
     tropomi.tropomi_process(start_date,end_date,lat_bounds,lon_bounds,coord_bin,
                             site=site,network=network,
@@ -78,3 +123,8 @@ if __name__=="__main__":
                             pressure_max_days=pressure_max_days,
                             name_directory=name_directory,
                             output_directory=output_directory)
+    
+    ################################
+    
+    # #print("Input parameters for tropomi function: ",tropomi_param)
+    #tropomi.tropomi_process(**tropomi_param)
