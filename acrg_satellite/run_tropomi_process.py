240745ab905fb174bfcff8eec3fa86d1e8926fc1
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2021-03-12 18:05:48 +0000

    Removing commented out section from run_tropomi_process code
    Former-commit-id: 7fe2078ae4e6e6ecf2eecd542417926b751d64d3

--- acrg_satellite/run_tropomi_process.py
+++ acrg_satellite/run_tropomi_process.py
@@ -6,12 +6,6 @@ Created on Fri Nov 13 17:33:47 2020
 @author: rt17603
 """
 
-#import acrg_satellite.gosat as gosat
-#import acrg_satellite.gosat_config as gosat_config
-#import os
-#import sys
-#import argparse
-
 import os
 import sys
 from pathlib import Path
@@ -25,7 +19,6 @@ except ImportError:
     acrg_path = os.getenv("ACRG_PATH")
 else:
     acrg_path = paths.acrg
-
 
 
 if __name__=="__main__":
@@ -73,92 +66,4 @@ if __name__=="__main__":
     
     print("Input parameters for tropomi function: ", tropomi_param)
     tropomi.tropomi_process(**tropomi_param)
-    
 
-
-
-    
-#     # tropomi_param = gosat_config.gosat_param(config_file)
-
-# #    if start_date is not None:
-# #      tropomi_param["start_date"] = start_date
-# #    if end_date is not None:
-# #      tropomi_param["end_date"] = end_date
-
-
-#     ####### TO BE REMOVED ONCE CONFIG FILE IS IN PLACE #####
-#     start_date = "2019-07-19"
-#     end_date = "2019-07-22"
-    
-#     area="GOSAT-BRAZIL"
-
-#     if area == "GOSAT-SOUTHAMERICA":
-#         ## Match to GOSAT-SOUTHAMERICA bounds
-#         lat_min = -57.0 # -56.98.071
-#         lat_max = +13.5 # +13.45861
-#         lon_min = -82.0 # -82.32861
-#         lon_max = -32.7 # -32.79305
-#     elif area == "GOSAT-BRAZIL":
-#         ## Match to GOSAT-BRAZIL bounds
-#         lat_min = -35.8 # -35.7525
-#         lat_max = +7.3  # +7.25138
-#         lon_min = -76.0 # -75.98444
-#         lon_max = -32.7 # -32.79305
-    
-#     dlat = 0.234
-#     dlon = 0.352
-# #    dlat = 0.10
-# #    dlon = 0.10
-
-#     lat_bounds = [lat_min,lat_max]
-#     lon_bounds = [lon_min,lon_max]
-#     coord_bin = [dlat,dlon]
-    
-# #    time_increment = "1min" # e.g. None, "10s", "30min" (always < 1 day)
-#     #time_increment = "30min" # e.g. None, "10s", "30min" (always < 1 day)
-#     time_increment = "1h" # e.g. None, "10s", "30min" (always < 1 day)
-#     #time_increment = None
-    
-#     site = area.replace("GOSAT-","TROPOMI-")
-#     network = "TROPOMI/"+site
-    
-#     write_name = True
-#     name_directory = Path("/home/rt17603/work/NAME_files/test")
-#     #name_directory = Path("/home/rt17603/work/NAME_files/")
-# #    name_directory = Path("/home/rt17603/work/NAME_files/03_bin")
-# #    name_directory = Path("/home/rt17603/work/NAME_files/05_bin")
-# #    name_directory = Path("/home/rt17603/work/NAME_files/035_1min_bin")
-#     #name_directory = Path("/home/rt17603/work/NAME_files/010_1min_bin")
-#     use_name_pressure = True
-#     pressure_domain = "SOUTHAMERICA"
-#     pressure_max_days = 1
-    
-#     output_directory = Path("/home/rt17603/work/obs/test")
-# #    output_directory = Path("/home/rt17603/work/obs/")
-#     #output_directory = Path("/home/rt17603/work/obs/03_bin")
-# #    output_directory = Path("/home/rt17603/work/obs/05_bin")
-# #    output_directory = Path("/home/rt17603/work/obs/035_1min_bin")
-#     #output_directory = Path("/home/rt17603/work/obs/010_1min_bin")
-    
-#     #max_points = None # Maximum number of points per NAME file
-#     max_points = 50 # Maximum number of points per NAME file
-    
-#     #regrid_method = "conservative_normed"
-#     regrid_method = "conservative"
-
-    
-#     tropomi.tropomi_process(start_date,end_date,lat_bounds,lon_bounds,coord_bin,
-#                             site=site,network=network,
-#                             time_increment=time_increment,write_name=write_name,
-#                             regrid_method=regrid_method,
-#                             max_points=max_points,
-#                             use_name_pressure=use_name_pressure,
-#                             pressure_domain=pressure_domain,
-#                             pressure_max_days=pressure_max_days,
-#                             name_directory=name_directory,
-#                             output_directory=output_directory)
-    
-#     ################################
-    
-#     # #print("Input parameters for tropomi function: ",tropomi_param)
-#     #tropomi.tropomi_process(**tropomi_param)
