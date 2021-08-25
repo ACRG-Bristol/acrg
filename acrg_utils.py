7f3a76b36996d7e413f1563ff93764be2706e23b
HannahChawner <hannah.chawner@bristol.ac.uk>
2021-08-25 15:40:18 +0100

    adapt combine_diff_resolution to automatically work out which data is high and low res
    Former-commit-id: f00c8c848a9dd556f2fb547a6ce59d0043ef5139

--- acrg_utils.py
+++ acrg_utils.py
@@ -12,49 +12,67 @@ def is_number(s):
         return False
     
 
-def combine_diff_resolution(data_low_res, data_high_res, method='multiply', verbose=True):
+def combine_diff_resolution(data_1, data_2, method='add', verbose=True):
     '''
     Combine datasets which have different time resolutions
+    Avoids having to resample data to the same resolution in order to add,
+    subtract, multiply, or divide them
     
     Args
-        data_low_res (xarray.DataArray or xarray.Dataset)
-            data with the lower time resolution
-        data_high_res (xarray.DataArray or xarray.Dataset)
-            data with the higher time resolution
+        data_1, data_2 (xarray.DataArray or xarray.Dataset)
+            Data to combine
+            Must have resolution of 1 hour, 1 day, 1 month, or 1 year
         method (str)
-            method by which to combine the datasets
-            defaults to 'multiply'
-            can be:
-                'multiply'      : data_low_res * data_high_res
-                'add'           : data_low_res + data_high_res
-                'subtract_high' : data_low_res - data_high_res
-                'subtract_low'  : data_high_res - data_low_res
-                'divide_by_high': data_high_res / data_low_res
-                'divide_by_low' : data_low_res / data_high_res
-        data_vars (dict)
-            variable names for the high and low res data
-            i.e. {'high_res': 'flux', 'low_res': 'flux'}
-        verbose (bool)
+            Method by which to combine the datasets
+            Can be:
+                'add':      data_1 + data_2 (default)
+                'subtract': data_1 - data_2
+                'multiply': data_1 * data_2
+                'divide':   data_1 / data_2
     
     Returns
         xarray.DataArray or xarray.Dataset
             has the same shape as resolved_data
     '''
-    # get the time step of the integrated data
-    # used for grouping the high time res data
-    time_step  = (data_low_res.time[1] - data_low_res.time[0]).values
-    time_scale = 'Month' if time_step.astype('timedelta64[M]').astype(int)>0 else \
-                 'Dayofyear' if time_step.astype('timedelta64[D]').astype(int)>0 else 'hour'
-    time_step  = time_step.astype(f'timedelta64[{time_scale[0]}]').astype(int)
+    if 'np' not in dir(): import numpy as np
+    data = {dd: dat for dd, dat in enumerate([data_1, data_2])}
+    # calculate the time step for each dataset
+    time_step  = {dd: (dat.time[1] - dat.time[0]).values for dd, dat in data.items()}
+    
+    if time_step[0]==time_step[1]:
+        # if the time steps are equal then apply the method as normal
+        if method.lower()=='multiply':
+            return data_1 * data_2
+        elif method.lower()=='add':
+            return data_1 + data_2
+        elif method.lower()=='subtract':
+            return data_1 - data_2
+        elif method.lower()=='divide':
+            return data_1 / data_2
+        else:
+            print(f'Method not recognised: {method}')
+            return(None)
+    
+    # work out which dataset is resolved and which is integrated
+    data_val = {'resolved'  : [dd for dd, step in time_step.items() if step==np.nanmin(np.array(list(time_step.values())))][0],
+                'integrated': [dd for dd, step in time_step.items() if step==np.nanmax(np.array(list(time_step.values())))][0]}
+    data = {res: data[dd] for res, dd in data_val.items()}
+    
+    # work out the time scale for each dataset
+    time_scale = {res: 'Month' if time_step[dd].astype('timedelta64[M]').astype(int)>0 else 
+                       'Dayofyear' if time_step[dd].astype('timedelta64[D]').astype(int)>0 else 'hour'
+                  for res, dd in data_val.items()}
+    time_step  = {res: time_step[dd].astype(f'timedelta64[{time_scale[res][0]}]').astype(int)
+                  for res, dd in data_val.items()}
     
     if verbose:
         print(f'Method to combine datasets: {method}',
-              f'\nIntegrated data has time step of {time_step} {time_scale.lower()}')
+              f'\nIntegrated data has time step of {time_step["integrated"]} {time_scale["integrated"].lower()}')
     
-    # define a function to apply to the resolved data
+    # function to apply to the resolved data
     def combine_method(resolved, method):
         # select the correct time slice from the integrated data
-        integrated = data_low_res.sel(time=resolved.time[0])
+        integrated = data['integrated'].sel(time=resolved.time[0])
         
         if method.lower()=='multiply':
             return resolved * integrated
@@ -64,15 +82,23 @@ def combine_diff_resolution(data_low_res, data_high_res, method='multiply', verb
             return integrated - resolved
         elif method.lower()=='subtract_low':
             return resolved - integrated
-        elif method.lower()=='divide_by_high':
-            return interated / resolved
-        elif method.lower()=='divide_by_low':
+        elif method.lower()=='divide_low':
+            # divide by the low res data
             return resolved / integrated
+        elif method.lower()=='divide_high':
+            return interated / resolved
         else:
-            print(f'Method not recognised')
+            print(f'Method not recognised: {method}')
+            return(None)
     
     # group the data by the same time scale as the integrated da
-    grouped_data = data_high_res.groupby(f'time.{time_scale.lower()}')
+    grouped_data = data['resolved'].groupby(f'time.{time_scale["integrated"].lower()}')
+    
+    # the required method depends on which order the data was given in
+    method = method if method in ['multiply', 'add'] else \
+             '_'.join([method, 'high']) if (data['integrated']==data_1).all() else \
+             '_'.join([method, 'low']) if (data['integrated']==data_2).all() else \
+             method
     
     # apply the combine method to each slice of the grouped data
     output = grouped_data.map(combine_method, method=method)
