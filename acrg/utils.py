405d6cb3745bb601d9ec29c602f39d139c281617
Matt Rigby <matt.rigby@bristol.ac.uk>
2021-09-14 05:49:43 +0100

    Moved file_decompression routine to acrg/utils
    Former-commit-id: 644c97df46463cfef21581144e717636232bf8bf

--- acrg/utils.py
+++ acrg/utils.py
@@ -1,4 +1,40 @@
+import gzip
+import bz2
+import os 
+import shutil
 
+from acrg.config.paths import paths
+acrg_path = paths.acrg
+
+def decompress_files(filepaths):
+    """ Decompress files at paths in filepaths
+
+        Args:
+            filepaths (list): List of paths to files to decompress
+        Returns:
+            list: List of paths to decompressed files
+    """
+    _, extension = os.path.splitext(filepaths[0])
+
+    if extension == ".bz2":
+        complib = bz2
+    elif extension == ".gz":
+        complib = gzip
+    else:
+        raise ValueError("Unable to decompress files other than bz2 and gz")
+        
+    decompressed_files = []
+   
+    for compressed_path in filepaths:
+        # Here decompressed_path will be the path minus the extension
+        decompressed_path, extension = os.path.splitext(compressed_path)
+
+        with complib.open(compressed_path, "rb") as f_in, open(decompressed_path, "wb") as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+        decompressed_files.append(decompressed_path)
+
+    return decompressed_files
 
 
 def is_number(s):
