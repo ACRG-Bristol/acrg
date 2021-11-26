c43ebd9ad8120b74491529749fa7fa1711d5094a
vf20487 <hannah.chawner@bristol.ac.uk>
2021-11-26 09:43:27 +0000

    add conversion for distance between lat and lon points in degrees to metres
    Former-commit-id: 6bca8d9bd83937e02b35263d306704e1db242622

--- acrg/convert.py
+++ acrg/convert.py
@@ -4,8 +4,9 @@ import json
 from acrg.obs.read import synonyms
 from acrg.config.paths import Paths
 
-
 acrg_path = Paths.acrg
+
+import numpy as np
 
 def molar_mass(species):
     '''
@@ -72,3 +73,69 @@ def convert_lons_0360(lons):
 
     return lons - div*360
 
+def latlon_distance(lat, lon, units=None, verbose=True):
+    '''
+    Get the distance between 2 points in km
+    
+    Inputs
+    ------
+    lat, lon: list
+        latitude and longitude of the 2 points, degrees
+    units: str, optional
+        units to return the distance in, defaults to km
+        should be 'm', 'metres', 'meters', 'km', 'kilometres', 'kilometers'
+    verbose: bool, optional
+        default: True
+    '''
+    latlon = {key: [np.deg2rad(ll) for ll in latlon_deg]
+              for key, latlon_deg in {'lat': lat, 'lon': lon}.items()}
+    d_latlon = {key: abs(ll[1] - ll[0]) for key, ll in latlon.items()}
+
+    radius_earth = 6378.137    # radius of the earth, km
+    radius_earth = radius_earth * 1e3 if units in ['m', 'metres', 'meters'] else radius_earth
+
+    # apply haversine formulae
+    a = (np.sin(d_latlon['lat'] / 2))**2
+    b = (np.sin(d_latlon['lon'] / 2))**2 * np.cos(latlon['lat'][0]) * np.cos(latlon['lat'][1])
+    c = 2 * np.arcsinh(np.sqrt(a+b))
+    return radius_earth * c
+
+def convert_degrees_metres(lat, d_lat, d_lon, units=None, verbose=True):
+    '''
+    Convert the size of a pixel from degrees to km
+    
+    Inputs
+    ------
+    lat: list or int or float
+        latitude of the centre of the pixel, degrees
+    d_lat, d_lon: int or float
+        size of the pixel, degrees
+    units: str, optional
+        units to return the distance in, defaults to km
+        should be 'm', 'metres', 'meters', 'km', 'kilometres', 'kilometers'
+    verbose: bool, optional
+        default: True
+    '''
+    units = 'km' if units is None else units
+    if verbose:
+        print(f'Converting to units of {units}')
+    
+    if units not in ['m', 'metres', 'meters', 'km', 'kilometres', 'kilometers']:
+        print('Please select units from: [m, metres, meters, km, kilometres, kilometers]')
+        return None
+    
+    radius_earth = 6378.137    # radius of the earth, km
+    radius_earth = radius_earth * 1e3 if units in ['m', 'metres', 'meters'] else radius_earth
+
+    # convert all values to radians
+    d_lat = np.deg2rad(d_lat)
+    d_lon = np.deg2rad(d_lon)
+    lat = np.deg2rad(lat)
+ 
+    # get the 2 points of latitude
+    lat = [lat - d_lat, lat + d_lat]
+ 
+    # apply haversine formulae
+    a = (np.sin(d_lat / 2))**2 + (np.sin(d_lon / 2))**2 * np.cos(lat[0]) * np.cos(lat[1])
+    c = 2 * np.arcsinh(np.sqrt(a))
+    return radius_earth * c
