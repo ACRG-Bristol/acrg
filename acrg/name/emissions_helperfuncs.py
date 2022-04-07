fb64e99262f8ae734fb368255c07e6e8a825963f
vf20487 <hannah.chawner@bristol.ac.uk>
2022-04-07 14:49:35 +0100

    update doc strings

--- acrg/name/emissions_helperfuncs.py
+++ acrg/name/emissions_helperfuncs.py
@@ -935,9 +935,11 @@ def embed_field(domain_field, country_field, country=None, country_file=None, do
     
     Args:
         domain_field, country_field (xarray DataArray)
-            domain and country flux fields
+            domain (large region) and country (regional) flux fields to be merged
+            country_field will be embedded into domain_field
         country (str, optional)
             name of the country associated with the country_field in the country_file
+            if not given, then a country mask will not be used
         domain (str, optional)
             domain associated with the domain_field
             used to look up a country mask file if country_file is None
@@ -947,7 +949,7 @@ def embed_field(domain_field, country_field, country=None, country_file=None, do
             country flux field are embedded into the domain field
         data_type (str, optional)
             data type to return (case insensitive)
-            either np_array, xr_dataset, or xr_array
+            either np_array, xr_dataset, or xr_array, defaults to xr_array
         crop (dict, optional)
             lat and lon to crop data to
             {'lat': [lat_1, lat_2], 'lon': [lon_1, lon_2]}
@@ -968,14 +970,6 @@ def embed_field(domain_field, country_field, country=None, country_file=None, do
     if verbose:
         print(f'Embedding {country} flux field into {domain}')
     
-    # check if there is a time dimension in both/any field array
-    time = True if all(['time' in dims for dims in [domain_field.dims, country_field.dims]]) else \
-           None if any(['time' in dims for dims in [domain_field.dims, country_field.dims]]) else False
-    if time is None:
-        # time dimension must be in both or neither array
-        print('Both fields must either be 2d or 3d')
-        return None 
-
     # crop data to given lat and lon if crop is not None
     data = {'domain': domain_field, 'country': country_field}
     if crop is not None and verbose:
@@ -1009,6 +1003,7 @@ def embed_field(domain_field, country_field, country=None, country_file=None, do
         with xr.open_dataset(country_file) as c_file:
             country_mask = c_file['country']
             country_codes = {country_name: cc for cc, country_name in enumerate(c_file['name'].values)}
+        
         # if country is uk then look for the named version in the country codes
         country = [cc for cc in list(country_codes.keys()) if 'united kingdom' in cc.lower()][0] \
                 if country.lower() in ['uk', 'united kingdom'] else country.upper()
@@ -1048,76 +1043,6 @@ def embed_field(domain_field, country_field, country=None, country_file=None, do
             print('Outputting xarray dataarray')
             
     return embedded_field
-
-def embed_UKGHG_EDGAR(edgar_flux, ukghg_flux, time=False, country=None, data_type='np_array',
-                      crop=None, global_attrs=None, verbose=True):
-    '''
-    Embed UKGHG map into the EDGAR map using a country mask
-    EDGAR and UKGHG map grid should match
-    
-    Args:
-        edgar_flux, ukghg_flux (xarray DataArray)
-            EDGAR and UKGHG flux maps
-        time (bool)
-            True if the flux data is 3d (lat, lon, time)
-        country (xarray DataArray, optional)
-            country mask
-            if None then the file country-ukmo_EUROPE.nc is used by default
-        data_type (str, optional)
-            data type to return (case insensitive)
-            either np_array, xr_array, or xr_dataset
-        crop (dict, optional)
-            lat and lon to crop data to
-            {'lat': [lat_1, lat_2], 'lon': [lon_1, lon_2]}
-        global_attrs (dict, optional)
-            global attributes to apply to the Dataset
-            only used if data_type is xr_dataset
-        verbose (bool, optional)
-            print update statements
-    
-    Returns:
-        xarray DataArray, xarray Dataset, or numpy array
-            map with UKGHG flux data embedded into EDGAR
-        
-    '''
-    if country is None:
-        country_file = os.path.join(data_path,'LPDM', 'countries', 'country-ukmo_EUROPE.nc')
-        with xr.open_dataset(country_file) as c_file:
-            country = c_file['country']
-    
-    if crop is not None:
-        edgar_flux = edgar_flux.sel(lat=slice(crop['lat'][0], crop['lat'][1]),
-                                    lon=slice(crop['lon'][0], crop['lon'][1]))
-        ukghg_flux = ukghg_flux.sel(lat=slice(crop['lat'][0], crop['lat'][1]),
-                                    lon=slice(crop['lon'][0], crop['lon'][1]))
-        country = country.sel(lat=slice(crop['lat'][0], crop['lat'][1]),
-                              lon=slice(crop['lon'][0], crop['lon'][1]))
-    
-    print('Embedding UKGHG into EDGAR map')
-    edgar_ukghg = copy.deepcopy(edgar_flux)
-    for lat in range(edgar_ukghg.shape[0]):
-        for lon in range(edgar_ukghg.shape[1]):
-            # For lat lon values where the country code is 19 (UK) replace EDGAR values with UKGHG
-            uk_country_code = 19.0
-            if country[lat,lon] == uk_country_code:
-                if time:
-                    edgar_ukghg[lat,lon,:] = ukghg_flux[lat,lon,:]
-                else:
-                    edgar_ukghg[lat,lon] = ukghg_flux[lat,lon]
-    
-    # convert data to required ouput type
-    if data_type.lower()=='np_array':
-        if verbose: print('Outputting numpy array')
-        edgar_ukghg = edgar_ukghg.values
-    elif data_type.lower()=='xr_dataset':
-        if verbose: print('Outputting xarray dataset')
-        global_attrs = {} if global_attrs is None else global_attrs
-        edgar_ukghg = edgar_ukghg.to_dataset(name='flux')
-        edgar_ukghg = edgar_ukghg.assign_attrs(global_attrs)
-    else:
-        if verbose: print('Outputting xarray dataarray')
-            
-    return edgar_ukghg
 
 def get_UKGHG_EDGAR(species,year,edgar_sectors=None,ukghg_sectors=None,
                      output_title=None,output_path=None):
